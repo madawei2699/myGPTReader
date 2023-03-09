8f8c9f4cbff64aa7205ed517fd7330a1b8bfb122
Dawei Ma <madawei2699@gmail.com>
2023-03-09 16:36:41 +0000

    WIP, try to fix llama_index issue

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 import re
 import os
 import openai
-from llama_index import GPTSimpleVectorIndex, TrafilaturaWebReader
-from llama_index.langchain_helpers.chatgpt import ChatGPTLLMPredictor
+from llama_index import GPTSimpleVectorIndex, LLMPredictor, TrafilaturaWebReader
+from langchain.chat_models import ChatOpenAI
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
@@ -39,9 +39,7 @@ def chat():
     message_normalized = insert_space(message)
     urls = extract_urls(message_normalized)
     if len(urls) > 0:
-        llm_predictor = ChatGPTLLMPredictor(temperature=0.2, prepend_messages=[
-            {"role": "system", "content": "你是一名全能专家，对各类知识都非常了解。在做分析时，请尽可能做出深度的分析，不要重复话术，并使用多种句式，使内容看起来更加有说服力。另外如果问题是中文的，你就用中文回复，如果是英文的，就用英文回复。"},
-        ])
+        llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
         documents = TrafilaturaWebReader().load_data(urls)
         index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor)
         response = index.query(message_normalized)
