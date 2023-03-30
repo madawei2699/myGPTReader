c89a387455bd9e024a960953212a338f17e6c0dc
madawei2699 <madawei2699@gmail.com>
2023-03-30 08:12:18 +0800

    fix: bugs

--- web/landing/www/src/components/Header.js
+++ web/landing/www/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { umamiEvent } from "../util";
 
 const Header = () => {
   const scrollToFeatures = () => {
@@ -15,6 +16,11 @@ const Header = () => {
       behavior: "smooth",
     });
   };
+
+  const goToGitHub = () => {
+    umamiEvent("GitHubFromMenu", { referrer })
+    window.location.href = "https://github.com/madawei2699/myGPTReader";
+  }
 
   return (
     <header className="bg-white shadow">
@@ -39,8 +45,8 @@ const Header = () => {
             Pricing
           </Link>
           <a
-            href="https://github.com/madawei2699/myGPTReader"
-            rel="noreferrer"
+            href="#"
+            onClick={goToGitHub}
             target="_blank"
             className="github-link ml-4 flex items-center justify-center w-10 h-10 rounded-full bg-gray-800 text-white font-semibold shadow-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
           >
