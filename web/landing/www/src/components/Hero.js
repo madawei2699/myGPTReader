c15b6f686d99f8aeb37f3c7f3cb17b2eedc723f6
madawei2699 <madawei2699@gmail.com>
2023-04-06 19:13:15 +0800

    feat: landing page, u

--- web/landing/www/src/components/Hero.js
+++ web/landing/www/src/components/Hero.js
@@ -30,7 +30,7 @@ const Hero = () => {
         </a>
         <p className="mt-4 italic text-gray-400">with more than 4000+ members to experience all these features for free.</p>
         {/* <img className="mt-12 mx-auto w-4/5" src="/hero.jpg" alt="Hero" /> */}
-        <img className="mt-12 mx-auto w-4/5" src="https://api.star-history.com/svg?repos=madawei2699/myGPTReader&type=Date" alt="myGPTReader" />
+        <img className="mt-12 mx-auto w-4/5" src="https://camo.githubusercontent.com/b7b1868e5ab7f73ea6473d930c06b05cbb371a189c43dcf7edfbeb30dc67c464/68747470733a2f2f6170692e737461722d686973746f72792e636f6d2f7376673f7265706f733d6d616461776569323639392f6d7947505452656164657226747970653d44617465" alt="myGPTReader" />
       </div>
     </section>
   );
