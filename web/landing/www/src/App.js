97ec8bc9c073e30bd301788d440b2cea3ffa2c70
madawei2699 <madawei2699@gmail.com>
2023-04-09 12:16:46 +0800

    fix: landing page, seo

--- web/landing/www/src/App.js
+++ web/landing/www/src/App.js
@@ -21,13 +21,13 @@ function App() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel='canonical' href='https://myreader.io/' />
+        <link rel='canonical' href='https://www.myreader.io/' />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:title" content="A new way to read with AI bot - myGPTReader" />
         <meta property="og:description" content="myGPTReader is a Slack bot that can read and summarize any webpage, documents including ebooks, or even videos from YouTube. It also can communicate with you via voice." />
         <meta property="og:image" content="/logo.png" />
-        <meta property="og:url" content="https://myreader.io/" />
+        <meta property="og:url" content="https://www.myreader.io/" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="myGPTReader" />
       </Helmet>
