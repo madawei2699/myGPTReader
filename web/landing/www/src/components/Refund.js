40c982cdf293a37d5f0985d32b41e6ac8fc17097
madawei2699 <madawei2699@gmail.com>
2023-04-22 20:08:40 +0800

    feat: landing page, u

--- web/landing/www/src/components/Refund.js
+++ web/landing/www/src/components/Refund.js
@@ -10,7 +10,7 @@ const Refund = () => {
          However, if you are not satisfied with our product, we offer a refund policy that ensures your rights as a customer.</p>
 
       <ul className='list-disc ml-5'>
-        <li>You have purchased myGPTReader within the last 30 days.</li>
+        <li>You have purchased myGPTReader within the last 7 days.</li>
         <li>You have encountered technical issues or problems with our product that we are unable to resolve within a reasonable time frame.</li>
         <li>You have not violated our terms of service or engaged in fraudulent activity.</li>
       </ul>
@@ -23,7 +23,7 @@ const Refund = () => {
           <>
               <div className='mt-5'>
                   <h3 className='text-lg font-semibold text-gray-700 mb-3'>Refund process:</h3>
-                  <p>To request a refund, please contact us at <span className='text-gray-700'>me@myreader.io</span> within 30 days of your purchase. We may ask you to provide additional information or documentation to support your refund request.</p>
+                  <p>To request a refund, please contact us at <span className='text-gray-700'>me@myreader.io</span> within 7 days of your purchase. We may ask you to provide additional information or documentation to support your refund request.</p>
                   <p>Once we receive your refund request, we will review your case and respond within 7 business days. If your refund request is approved, we will issue a refund to the original payment method you used to purchase myGPTReader.</p>
               </div>
 
@@ -32,7 +32,7 @@ const Refund = () => {
                 <ul className='list-disc ml-5'>
                     <li>We do not offer refunds for any purchases made outside of our website or authorized resellers.</li>
                     <li>We reserve the right to refuse a refund request if we suspect fraudulent activity or violation of our terms of service.</li>
-                    <li>We do not offer refunds for purchases made more than 30 days ago.</li>
+                    <li>We do not offer refunds for purchases made more than 7 days ago.</li>
                 </ul>
               </div>
 
